3e6b45e43513355aa8cc596052b3dc6c4af8acca
chorm <120085336@qq.com>
2021-01-06 17:35:57 +0800

    commit

---
+++ src/db/EventManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KMS.src.db
+{
+    /// <summary>
+    /// create at 2021-01-06 17:11
+    /// </summary>
+    class EventManager
+    {
+        private static EventManager instance;
+        internal static EventManager GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new EventManager();
+
+                return instance;
+            }
+        }
+
+        private EventManager()
+        {
+        
+        }
+    }
+}
